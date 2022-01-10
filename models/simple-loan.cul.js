b31a73fee7dffd1feaf9be98b7018008a96d173f
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-01-10 21:48:25 +0000

    readability changes

--- models/simple-loan.cul.js
+++ models/simple-loan.cul.js
@@ -4,15 +4,16 @@ export const v_pow_term = () => Math.pow(v(), term());
 
 export const repayment_amount = () => (principal() * i()) / (1 - v_pow_term());
 
+export const interest = () => balance({ year_in: year() - 1 }) * i();
+
+// modelling all repayments as being met:
+export const repayment_made = () => year() <= term() && year() != 0;
+export const repayment = () => repayment_made() * repayment_amount();
+
 export const balance = () => {
   if (year() == 0) return principal();
   else return balance({ year_in: year() - 1 }) + interest() - repayment();
 };
-
-export const repayment_made = () => year() <= term() && year() != 0;
-export const repayment = () => repayment_made() * repayment_amount();
-
-export const interest = () => balance({ year_in: year() - 1 }) * i();
 
 // inputs:
 export const principal = () => principal_in;
--- models/use-simple-loan.js
+++ models/use-simple-loan.js
@@ -13,10 +13,10 @@ console.log(
 );
 
 console.log(
-  _.range(101).map(
+  _.range(481).map(
     (year_in) =>
       +l
-        .balance({ year_in, term_in: 100, i_in: 0.04, principal_in: 100000 })
+        .balance({ year_in, term_in: 480, i_in: 0.04, principal_in: 100000 })
         .toFixed(2)
   )
 );
